aff26668c346672c3af3a872861286318a06301b
benzkji <bnzk@bnzk.ch>
2014-10-25 00:03:07 +0200

    more testing stuff

---
+++ folderless/tests/test_settings.py
@@ -0,0 +1,69 @@
+"""Settings that need to be set in order to run the tests."""
+import os
+import logging
+import django.conf.global_settings as DEFAULT_SETTINGS
+
+DEBUG = True
+
+logging.getLogger("factory").setLevel(logging.WARN)
+
+SITE_ID = 1
+
+APP_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..'))
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+ROOT_URLCONF = 'folderless.tests.urls'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(APP_ROOT, '../app_static')
+MEDIA_ROOT = os.path.join(APP_ROOT, '../app_media')
+STATICFILES_DIRS = (
+    os.path.join(APP_ROOT, 'static'),
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(APP_ROOT, 'tests/test_app/templates'),
+)
+
+COVERAGE_REPORT_HTML_OUTPUT_DIR = os.path.join(
+    os.path.join(APP_ROOT, 'tests/coverage'))
+COVERAGE_MODULE_EXCLUDES = [
+    'tests$', 'settings$', 'urls$', 'locale$',
+    'migrations', 'fixtures', 'admin$', 'django_extensions',
+]
+
+EXTERNAL_APPS = [
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
+    'django_nose',
+]
+
+INTERNAL_APPS = [
+    'painless_redirects',
+    'painless_redirects.tests.test_app',
+]
+
+MIDDLEWARE_CLASSES = DEFAULT_SETTINGS.MIDDLEWARE_CLASSES + (
+    "painless_redirects.middleware.ManualRedirectMiddleware",
+    "painless_redirects.middleware.ForceSiteDomainRedirectMiddleware",
+)
+
+INSTALLED_APPS = EXTERNAL_APPS + INTERNAL_APPS
+COVERAGE_MODULE_EXCLUDES += EXTERNAL_APPS
+
+SECRET_KEY = 'foobar'
