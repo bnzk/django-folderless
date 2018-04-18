93aafde6c9f9007ebf5e8a5885818a9b4da384ce
benzkji <bnzk@bnzk.ch>
2018-04-18 08:32:07 +0200

    go

--- folderless/utils.py
+++ folderless/utils.py
@@ -20,11 +20,14 @@ def model_get_all_related_objects(model):
     """
     https://docs.djangoproject.com/en/2.0/ref/models/meta/
     """
-    return [
-        f for f in model._meta.get_fields()
-        if (f.one_to_many or f.one_to_one)
-        and f.auto_created and not f.concrete
-    ]
+    if getattr(model._meta, 'get_all_related_objects'):
+        return model._meta.get_all_related_objects()
+    else:
+        return [
+            f for f in model._meta.get_fields()
+            if (f.one_to_many or f.one_to_one)
+            and f.auto_created and not f.concrete
+        ]
 
 
 def sha1_from_file(file):
