97ed95fed4a42408ff479540917e1046b1543a07
benzkji <bnzk@bnzk.ch>
2018-11-19 10:58:58 +0100

    admin onyl jquery mode

--- folderless/fields.py
+++ folderless/fields.py
@@ -102,7 +102,7 @@ class FolderlessFileWidget(ForeignKeyRawIdWidget):
     class Media:
         js = (
             # 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            settings.FOLDERLESS_STATIC_URL + 'js/vendor/jquery-1.9.1.min.js',
+            # settings.FOLDERLESS_STATIC_URL + 'js/vendor/jquery-1.9.1.min.js',
             settings.FOLDERLESS_STATIC_URL + 'js/jquery_pre_init.js',  # for the moment!
             settings.FOLDERLESS_STATIC_URL + 'js/vendor/jquery.ui.widget.js',
             settings.FOLDERLESS_STATIC_URL + 'js/vendor/jquery.iframe-transport.js',
--- folderless/static/folderless/js/jquery_pre_init.js
+++ folderless/static/folderless/js/jquery_pre_init.js
@@ -4,24 +4,12 @@ var folderless = folderless || {};
 if (typeof django !== 'undefined' && typeof django.jQuery !== 'undefined') {
     // If django.jQuery is already defined, use it.
     folderless.jQuery = django.jQuery;
-}
-else {
-    // We include jquery itself in our widget's media, because we need it.
-    // Normally, we expect our widget's reference to admin/js/vendor/jquery/jquery.js
-    // to be skipped, because django's own code has already included it.
-    // However, if django.jQuery is NOT defined, we know that jquery was not
-    // included before we did it ourselves. This can happen if we're not being
-    // rendered in a django admin form.
-    // However, someone ELSE'S jQuery may have been included before ours, in
-    // which case we must ensure that our jquery doesn't override theirs, since
-    // it might be a newer version that other code on the page relies on.
-    // Thus, we must run jQuery.noConflict(true) here to move our jQuery out of
-    // the way.
-    folderless.jQuery = jQuery.noConflict(true);
+} else {
+    console.log("folderless field is only supported in django admin!");
 }
 
 // In addition to all of this, we must ensure that the global jQuery and $ are
-// defined, because Select2 requires that. jQuery will only be undefined at
+// defined, because jqueryUI requires that. jQuery will only be undefined at
 // this point if only we or django included it.
 if (typeof jQuery === 'undefined') {
     jQuery = folderless.jQuery;
